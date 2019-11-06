8580b0dc46a8ebf95be3f5ec3f68ba66cf1e3ff1
Benjamin Wu <benjaminwu124@gmail.com>
2019-11-06 10:23:09 -0500

    Create replacement for Athena error_aggregates

---
+++ sql/telemetry/error_aggregates/query.sql
@@ -0,0 +1,60 @@
+CREATE TEMP FUNCTION udf_round_timestamp_to_minute(timestamp_expression TIMESTAMP, minute INT64) AS (
+  TIMESTAMP_SECONDS(
+    CAST((FLOOR(UNIX_SECONDS(timestamp_expression) / (minute * 60)) * minute * 60) AS INT64)
+  )
+);
+--
+SELECT
+  COUNT(*) AS count,
+  udf_round_timestamp_to_minute(submission_timestamp, 5) AS window_start,
+  normalized_channel AS channel,
+  environment.build.version,
+  application.display_version,
+  application.build_id,
+  normalized_app_name AS app_name,
+  normalized_os AS os,
+  normalized_os_version AS os_version,
+  application.architecture,
+  normalized_country_code AS country,
+  COUNT(*) AS count,
+  SUM(
+    IF(
+      payload.process_type = 'main'
+      OR payload.process_type IS NULL,
+      1,
+      0
+    )
+  ) AS main_crash,
+  SUM(
+    IF(
+      REGEXP_CONTAINS(payload.process_type, 'content'),
+      1,
+      0
+    )
+  ) AS content_crash,
+  SUM(IF(payload.metadata.startup_crash = '1', 1, 0)) AS startup_crash,
+  SUM(
+    IF(
+      REGEXP_CONTAINS(
+        payload.metadata.ipc_channel_error,
+        'ShutDownKill'
+      ),
+      1,
+      0
+    )
+  ) AS content_shutdown_crash
+FROM
+  `moz-fx-data-shared-prod.telemetry_live.crash_v4`
+WHERE
+  DATE(submission_timestamp) = "2019-11-04" -- TODO: replace with parameter
+GROUP BY
+  window_start,
+  normalized_channel,
+  environment.build.version,
+  application.display_version,
+  application.build_id,
+  normalized_app_name,
+  normalized_os,
+  normalized_os_version,
+  application.architecture,
+  normalized_country_code
---
+++ templates/telemetry/error_aggregates/query.sql
@@ -0,0 +1,54 @@
+SELECT
+  COUNT(*) AS count,
+  udf_round_timestamp_to_minute(submission_timestamp, 5) AS window_start,
+  normalized_channel AS channel,
+  environment.build.version,
+  application.display_version,
+  application.build_id,
+  normalized_app_name AS app_name,
+  normalized_os AS os,
+  normalized_os_version AS os_version,
+  application.architecture,
+  normalized_country_code AS country,
+  COUNT(*) AS count,
+  SUM(
+    IF(
+      payload.process_type = 'main'
+      OR payload.process_type IS NULL,
+      1,
+      0
+    )
+  ) AS main_crash,
+  SUM(
+    IF(
+      REGEXP_CONTAINS(payload.process_type, 'content'),
+      1,
+      0
+    )
+  ) AS content_crash,
+  SUM(IF(payload.metadata.startup_crash = '1', 1, 0)) AS startup_crash,
+  SUM(
+    IF(
+      REGEXP_CONTAINS(
+        payload.metadata.ipc_channel_error,
+        'ShutDownKill'
+      ),
+      1,
+      0
+    )
+  ) AS content_shutdown_crash
+FROM
+  `moz-fx-data-shared-prod.telemetry_live.crash_v4`
+WHERE
+  DATE(submission_timestamp) = "2019-11-04" -- TODO: replace with parameter
+GROUP BY
+  window_start,
+  normalized_channel,
+  environment.build.version,
+  application.display_version,
+  application.build_id,
+  normalized_app_name,
+  normalized_os,
+  normalized_os_version,
+  application.architecture,
+  normalized_country_code
